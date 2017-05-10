0c375c4f7817fa08e1f75446d6e994848b5c2584
antoinerobin <antoine.robin.2014@utt.fr>
2017-05-10 15:16:51 +0200

    add some logger

--- model/Export/AbstractQTIItemExporter.php
+++ model/Export/AbstractQTIItemExporter.php
@@ -71,7 +71,7 @@ abstract class AbstractQTIItemExporter extends taoItems_models_classes_ItemExpor
      */
     public function export($options = array())
     {
-        $report = \common_report_Report::createSuccess();
+        $report = \common_report_Report::createSuccess(__('Item %s successfully exported', $this->getItem()->getLabel()));
         $asApip = isset($options['apip']) && $options['apip'] === true;
         
         $lang = \common_session_SessionManager::getSession()->getDataLanguage();
